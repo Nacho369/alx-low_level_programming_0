d42f2ebed2616ce9cf9623440134ab1ecfaa70aa
Fortune Iheanacho <nachodev369@gmail.com>
2022-09-08 06:59:29 -0700

    Task 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry Point
  *
- * Description: Prints "with proper grammar, but the outcome is a piece of art,"
+ * Description: Prints "with proper grammar, but the outcome is a
+ * piece of art,"
  *
  * Return: 0 (Success)
  */
