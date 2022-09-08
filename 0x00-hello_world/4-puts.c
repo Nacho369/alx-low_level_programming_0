8b3f497a626776805f81058c5bc78df82b1469bf
Fortune Iheanacho <nachodev369@gmail.com>
2022-09-08 06:04:36 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * This function prints "Programming is like building a multilingual puzzle"
+ *
+ * Returns 0 (Success)
+*/
+
 int main(void)
 {
-	/* This prints the statement shown in the quotes below*/
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
