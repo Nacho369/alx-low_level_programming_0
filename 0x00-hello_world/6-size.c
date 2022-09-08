dc2b24bf98fbcecd68f479da9c8bc987514a065f
Fortune Iheanacho <nachodev369@gmail.com>
2022-09-08 09:29:27 -0700

    Task 6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry Point
  *
- * Description:  Prints the size of various types on the computer
- * it is compiled and run on.
+ * Description:  Prints the size of various
+ * types on the computer it is compiled and run on.
  *
  * Return: 0 (Success)
  */
@@ -15,6 +15,6 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-	
+
 	return (0);
 }
