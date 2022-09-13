71f098d42572e2b423d323fe3ea16643d954d60c
Nacho369 <nachodev369@gmail.com>
2022-09-13 06:54:30 -0700

    Task 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: A program that prints _putchar, followed by a new line.
+ *
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+This is a README file for C Functions and Nested Loops.
+
+To be updated...
