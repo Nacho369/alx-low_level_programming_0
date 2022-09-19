b959194d09a03ccfdac620787e52bf465f4060b6
Nacho369 <nachodev369@gmail.com>
2022-09-19 13:27:38 -0700

    Task 10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,40 +9,6 @@
  */
 int _atoi(char *s)
 {
-	/*
-	int c, sign, offset;
-	unsigned int n;
-
-	
-	if (s[0] == '-')
-	{
-		sign = -1;
-	}
-
-	if (sign == -1)
-	{
-		offset = 1;
-	}
-	else
-	{
-		offset = 0;
-	}
-
-	n = 0;
-
-	for (c = offset; s[c] != '\0'; c++)
-	{
-		n = n * 10 + s[c] - '0';
-	}
-
-	if (sign == -1)
-	{
-		n = -n;
-	}
-
-	return (n);
-	*/
-
 	int sign = 1;
 	unsigned int num = 0;
 
