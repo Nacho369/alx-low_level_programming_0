3195a4aabf9dc758442475704b1ee9d8327b427f
Nacho369 <nachodev369@gmail.com>
2022-09-21 06:11:40 -0700

    Task 3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares to string to see if they are equal
+ *
+ * @s1: First string to compare
+ * @s2: Second string to compare
+ *
+ * Return: 0, if the s1 and s2 are equal; 
+ * a negative value if s1 is less than s2;
+ * a positive value if s1 is greater than s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
