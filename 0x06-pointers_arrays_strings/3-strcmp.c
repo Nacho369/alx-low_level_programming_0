025bb6f7c26e4e376672e3cb20f080cf24a62ade
Nacho369 <nachodev369@gmail.com>
2022-09-21 09:07:26 -0700

    Task 3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s1: First string to compare
  * @s2: Second string to compare
  *
- * Return: 0, if the s1 and s2 are equal; 
+ * Return: 0, if the s1 and s2 are equal;
  * a negative value if s1 is less than s2;
  * a positive value if s1 is greater than s2.
  */
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverse the content of an array if integers
+ *
+ * @a: array variable
+ * @n: Number of element of array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+
+}
