d914e4915763753e2c8093ae9482fbc035824d95
Nacho369 <nachodev369@gmail.com>
2022-09-30 06:58:45 -0700

    Task 3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Multiplies to number passed in as
+ * arguments
+ *
+ * @argc: The number of arguments supplied to the
+ * program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 (Success) if program receives two
+ * argument else 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
