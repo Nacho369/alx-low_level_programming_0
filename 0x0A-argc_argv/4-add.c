1a507fe60210559fab14d7cd3b2d495ab5ec0b58
Nacho369 <nachodev369@gmail.com>
2022-09-30 09:04:01 -0700

    Task 3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,16 +15,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int result;
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
 
 	if (argc != 3)
 	{
-		printf("Error\n")
+		printf("Error\n");
+
 		return (1);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
 	result = num1 * num2;
 
 	printf("%d\n", result);
