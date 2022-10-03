9052ec014e664f9986fefc309bd32fb7dffbb161
Nacho369 <nachodev369@gmail.com>
2022-10-03 08:21:49 -0700

    Task 0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	static char *ptr;
 
+	if (size == 0)
+		return (NULL);
+
 	ptr = (char *) malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
