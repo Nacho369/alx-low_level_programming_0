91e878a0b88961ba74beeb048262ff8d27ca59d9
Nacho369 <nachodev369@gmail.com>
2022-10-06 09:01:39 -0700

    Task 1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,22 +24,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i]; i++)
-		len1++;
+	for (len1 = 0; s1[len1]; len1++)
+		;
 
-	for (j = 0; s2[j]; j++)
-		len2++;
+	for (len2 = 0; s2[len2]; len2++)
+		;
 
-	if (n <= len2)
-	{
+	if (len2 > n)
 		len2 = n;
-		len = len1 + len2;
-	}
-	else if (n >= len2)
-	{
-		n = len2;
-		len = len1 + n;
-	}
+
+	len = len1 + len2;
 
 	ptr = malloc(sizeof(char) * (len + 1));
 
