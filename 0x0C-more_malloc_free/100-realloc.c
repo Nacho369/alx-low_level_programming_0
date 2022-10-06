ee8d4155796af3252ce8f0e54ee053a25419298d
Nacho369 <nachodev369@gmail.com>
2022-10-06 15:43:14 -0700

    Task 4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -32,7 +32,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 	}
 
-	if (new_size > old_size)
+	if (new_size > old_size && ptr != NULL)
 	{
 		realloc = malloc(new_size);
 
@@ -48,7 +48,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-
 		return (NULL);
 	}
 
