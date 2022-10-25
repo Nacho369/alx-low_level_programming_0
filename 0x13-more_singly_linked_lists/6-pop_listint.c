f74e8e5fa0c62f9831912d3d9c4089de3d3d428c
Nacho369 <nachodev369@gmail.com>
2022-10-25 12:28:33 -0700

    Task 6. Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,5 +2,16 @@
 
 int pop_listint(listint_t **head)
 {
+	int i;
+	listint_t *ptr = NULL;
 
+	if (*head == NULL)
+		return (0);
+
+	ptr = *head;
+	i = ptr->n;
+	*head = ptr->next;
+	free(ptr);
+
+	return (i);
 }
