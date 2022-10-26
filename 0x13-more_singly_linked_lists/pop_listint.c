cd87bd7325bc886236e43f614fd2f1ec9ffe31dd
Nacho369 <nachodev369@gmail.com>
2022-10-25 18:41:13 -0700

    Task 10. Delete at index

--- 0x13-more_singly_linked_lists/pop_listint.c
+++ 0x13-more_singly_linked_lists/pop_listint.c
@@ -1,24 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
  * pop_listint - delete the head node and update the linked list
- *
  * @head: first element of the list
- *
- * Return: Head node data.
+ * Return: no return
  */
 int pop_listint(listint_t **head)
 {
+	struct listint_s *temp = NULL;
 	int i;
-	listint_t *ptr = NULL;
 
 	if (*head == NULL)
 		return (0);
-
-	ptr = *head;
-	i = ptr->n;
-	*head = ptr->next;
-	free(ptr);
+	temp = *head;
+	i = temp->n;
+	*head = temp->next;
+	free(temp);
 
 	return (i);
 }
