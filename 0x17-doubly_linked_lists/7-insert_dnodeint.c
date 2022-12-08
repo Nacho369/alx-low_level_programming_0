aa70d14fa8943bc141136734aa7ff245a23004dd
Nacho369 <nachodev369@gmail.com>
2022-12-08 14:38:13 -0800

    Task 7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	for (node = 0; ptr != NULL; node++)
+	for (node = 0; ptr != NULL || node + 1 == idx; node++)
 	{
 		if (node + 1 == idx)
 		{
