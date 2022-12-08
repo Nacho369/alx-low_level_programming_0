449c26f44474b92c2335ede2bf67efebb1222924
Nacho369 <nachodev369@gmail.com>
2022-12-08 07:01:58 -0800

    Task 0. Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all the elements of a 
+ * dlistint_t list.
+ *
+ * @h: Pointer to head of the list
+ *
+ * Return: Number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *ptr = h;
+	size_t nodes;
+
+	if (h == NULL)
+		return (0);
+
+	for (nodes = 0; ptr != NULL; nodes++)
+	{
+		printf("%d\n", ptr->n);
+
+		ptr = ptr->next;
+	}
+
+	return (nodes);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,5 @@
+# This directory contains files for C - Doubly linked lists
+
+## 0x17. C - Doubly linked lists
+
+To be edited soon...
