247054f72e2b0f29fd98555ee70e27eee7be5c2d
Nacho369 <nachodev369@gmail.com>
2022-12-08 08:28:10 -0800

    Task 3. Add node at the end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -31,7 +31,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	while (ptr->next != NULL)
 		ptr = ptr->next;
-	
+
 	ptr->next = new_node;
 	new_node->prev = ptr;
 	new_node->next = NULL;
