74dcb9635fb28904e8b45cb35d7a27f3447d1db0
Nacho369 <nachodev369@gmail.com>
2022-12-08 07:03:23 -0800

    Task 0. Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a 
+ * print_dlistint - Prints all the elements of a
  * dlistint_t list.
  *
  * @h: Pointer to head of the list
